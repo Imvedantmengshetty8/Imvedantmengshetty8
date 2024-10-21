665c6df7d5376b9505d8e9a858c9953653abc59a
Imvedantmengshetty8 <mengshetty.vedant174@gmail.com>
2024-10-21 10:55:52 +0530

    Create Hello.C

---
+++ Hello.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+int main()
+
+{
+
+    int a,b,c;
+
+    
+
+    a=4;
+
+    b=6;
+
+    c=a*b;
+
+    printf("%d",c=a*b);
+
+    
+
+    printf(" Welcome to GDBOnline.");
+
+    
+
+    return 0;
+
+}
