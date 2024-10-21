63d55d5e9dce06a30bb1b85440941cc0bce7e21b
Imvedantmengshetty8 <mengshetty.vedant174@gmail.com>
2024-10-21 15:57:13 +0530

    Arithmeticoperations.c

--- Arithmeticoperations.c
+++ Arithmeticoperations.c
@@ -1,4 +1,52 @@
 #include <stdio.h>
+    
+int main()
+{
+    int a,b,c;
+    
+    a=56;
+    b=26;
+    c=a+b;
+    printf("%d",c=a+b);
+    
+    return 0;
+}
+Output:
+82
+
+...Program finished with exit code 0
+Press ENTER to exit console.
+
+
+
+
+    
+    
+#include <stdio.h>
+
+  int main()
+{
+    int a,b,c;
+    
+    a=53;
+    b=79;
+    c=a-b;
+    printf("%d",c=a-b);
+    
+    return 0;
+}
+
+output:
+-26
+
+...Program finished with exit code 0
+Press ENTER to exit console.
+
+  
+    
+    
+    
+#include <stdio.h>
 
 int main()
 {
@@ -18,3 +66,96 @@ output;
 
 ...Program finished with exit code 0
 Press ENTER to exit console.#include <stdio.h>
+
+
+
+
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    
+    a=40;
+    b=5;
+    c=a/b;
+    printf("%d",c=a/b);
+    
+    return 0;
+}
+
+
+output:
+
+
+
+
+
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    a=5;
+    b=100;
+    c=a%b;
+    printf("%d", c=a%b);
+    return 0;
+}
+
+
+Output:
+
+5
+[Process completed - press Enter]
+
+
+
+
+
+
+
+    
+    
+    
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    
+    a=12;
+    b=8;
+    
+    printf("a+b = %d\n",a+b);
+    printf("a-b = %d\n",a-b);
+    printf("a*b = %d\n",a*b);
+    printf("a/b = %d\n",a/b);
+    printf("b>a = %d\n",b>a);
+    printf("a==b= %d\n",a==b);
+    printf("a>b = %d\n",a>b);
+    
+    return 0;
+}
+
+output:
+
+a+b = 20
+a-b = 4
+a*b = 96
+a/b = 1
+b>a = 0
+a==b= 0
+a>b = 1
+
+
+...Program finished with exit code 0
+Press ENTER to exit console.
+
+
+
+
+    
