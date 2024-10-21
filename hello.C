0812c2b9523a774102a83fef00576cca9d16e98b
Imvedantmengshetty8 <mengshetty.vedant174@gmail.com>
2024-10-21 15:04:56 +0530

    Update and rename Hello.C to hello.C

---
+++ hello.C
@@ -0,0 +1,52 @@
+
+#include<stdio.h>
+
+int main()
+{
+    printf("hello world");
+    return 0;
+}
+
+
+Output:
+hello world
+[Process completed - press Enter]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
