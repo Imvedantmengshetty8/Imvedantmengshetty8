023d146f203b7f4827594e33f59a5b68c3b7ecdb
Imvedantmengshetty8 <mengshetty.vedant174@gmail.com>
2024-10-21 15:37:22 +0530

    Arithmeticoperations.c

---
+++ Arithmeticoperations.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    
+    a=4;
+    b=6;
+    c=a*b;
+    printf("%d",c=a*b);
+    
+    printf(" Welcome to GDBOnline.");
+    
+    return 0;
+}
+output;
+24 Welcome to GDBOnline.
+
+...Program finished with exit code 0
+Press ENTER to exit console.#include <stdio.h>
