4d4a9ffcf09dbe032410bb84e245524948494b81
Imvedantmengshetty8 <mengshetty.vedant174@gmail.com>
2024-10-28 01:54:24 +0530

    Create Assignmentoperators.C

---
+++ Assignmentoperators.C
@@ -0,0 +1,64 @@
+//code for assignment operators //
+#include <stdio.h>
+
+int main() {
+     int main;
+     int p,q;
+     p= 36;
+     q= 64;
+     
+     //operation for addition assignment//
+    printf("p+q=%d\n",p+q);
+    //operation for subtraction assignment//
+    printf("p-q=%d\n",p-q);
+    //operation for division assignment//
+    printf("p/q=%d\n",p/q);
+    //operation for multiplication assignment//
+    printf("p*q=%d\n",p*q);
+    //operation for modulus assignment//
+    printf("p%q=%d\n",p%q);
+    //operations for assignment //
+    printf("p=q=%d\n",p=q);
+    
+    return 0;
+}
+
+
+Output:
+
+
+p+q=100
+p-q=-28
+p/q=0
+p*q=2304
+p=36
+p=q=64
+
+[Process completed - press Enter]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
