35ff625b73c5c8e6f9ff5d1743ef312d3397b76c
Imvedantmengshetty8 <mengshetty.vedant174@gmail.com>
2024-10-27 17:31:22 +0530

    Datatypes.C

---
+++ Datatypes.C
@@ -0,0 +1,40 @@
+1.primary data types 
+
+//program for primary datatype float//
+#include<stdio.h>
+
+int main() 
+{
+     float i;
+     i=18.8;
+     
+     printf("%f", i);
+     return 0;
+     
+     }
+
+
+
+
+output:
+
+18.799999
+[Process completed - press Enter]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
