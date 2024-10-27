cbb7f586deb8375f446e1a9d81d480fb5db3b530
Imvedantmengshetty8 <mengshetty.vedant174@gmail.com>
2024-10-28 02:30:16 +0530

    Create Relationaloperators.C

---
+++ Relationaloperators.C
@@ -0,0 +1,79 @@
+//code for relational operators //
+#include <stdio.h>
+
+int main() {
+     int main;
+     int m,n;
+     m= 25;
+     n= 75;
+     
+     //operation for lesser//
+    printf("m<n=%d\n",m<n);
+    //operation for greater //
+    printf("m>n=%d\n",m>n);
+    //operation for lesser than //
+    printf("m<=n=%d\n",m<=n);
+    //operation for greater than //
+    printf("m>=n=%d\n",m>=n);
+    //operation for equal to//
+    printf("m=n=%d\n",m=n);
+    //operations for not equal to//
+    printf("m!=n=%d\n",m!=n);
+    //operation for double equal to//
+    printf("m==n=%d\n",m==n);
+    
+    return 0;
+}
+
+
+Output:
+
+m<n=1
+m>n=0
+m<=n=1
+m>=n=0
+m=n=75
+m!=n=0
+m==n=1
+
+[Process completed - press Enter]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
