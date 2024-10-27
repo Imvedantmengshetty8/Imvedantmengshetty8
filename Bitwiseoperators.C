59194710f50ec0bbc937f2e7ff394b3c6d835590
Imvedantmengshetty8 <mengshetty.vedant174@gmail.com>
2024-10-28 04:19:52 +0530

    Create Bitwiseoperators.C

---
+++ Bitwiseoperators.C
@@ -0,0 +1,128 @@
+//code for bitwise operators //
+
+1.bitwose AND operation 
+
+
+
+
+
+#include <stdio.h>
+
+int main() {
+     
+     int a,b;
+     a= 12;//{00001100[in binary]//
+     b= 25;//{00011001[in binary]//
+     
+     //operation for bitwise AND//
+    printf("output=%d",a&b);
+    
+    
+    return 0;
+}
+
+
+
+
+Output:
+
+
+output=8
+
+00001000 =8
+[Process completed - press Enter]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+2.bitwise OR operation 
+
+
+
+
+#include <stdio.h>
+
+int main() {
+     
+     int a,b;
+     a= 12;//{00001100[in binary]//
+     b= 25;//{00011001[in binary]//
+     
+     //operation for bitwise OR//
+    printf("D=%d",a|b);
+    
+    
+    return 0;
+}
+
+
+
+
+
+Output:
+
+
+D=29
+00011101=29
+[Process completed - press Enter]
+
+
+
+
+
+
+
+
+
+
+
+
+
+3.bitwise XOR operation 
+
+
+
+
+
+//code for bitwise operators //
+#include <stdio.h>
+
+int main() {
+     
+     int a,b;
+     a= 12;//{00001100[in binary]//
+     b= 25;//{00011001[in binary]//
+     
+     //operation for bitwise XOR//
+    printf("D=%d",a^b);
+    
+    
+    return 0;
+}
+
+
+
+
+
+
+Output:
+
+
+D=21
+00010101=21
+[Process completed - press Enter]
+
+
+
+
