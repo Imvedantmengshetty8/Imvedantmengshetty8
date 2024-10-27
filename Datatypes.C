5b2d1a75b6887245db021563a1acdd1d3aacaac7
Imvedantmengshetty8 <mengshetty.vedant174@gmail.com>
2024-10-27 18:27:47 +0530

    Datatypes.C

--- Datatypes.C
+++ Datatypes.C
@@ -24,13 +24,52 @@ output:
 
 
 
+2. Derived data types
+
+// Program to take 7 values from the user and store them in an array
+// Print the elements stored in the array
+
+#include <stdio.h>
+
+int main() {
+
+  int values[7];
+
+  printf("Enter 7 integers: ");
+
+  // taking input and storing it in an array
+  for(int i = 0; i < 7; ++i) {
+     scanf("%d", &values[i]);
+  }
+
+  printf("Displaying integers: ");
+
+  // printing elements of an array
+  for(int i = 0; i < 7; ++i) {
+     printf("%d\n", values[i]);
+  }
+  return 0;
+}
 
 
+Output:
 
+Enter 7 integers: 1
+37
+-5
+0
+2
+3
+7
+Displaying integers: 1
+37
+-5
+0
+2
+3
+7
 
-
-
-
+[Process completed - press Enter]
 
 
 
