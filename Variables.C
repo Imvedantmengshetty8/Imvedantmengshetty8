224c43e46f0ceb44f08535be0fa70c95c07ba571
Imvedantmengshetty8 <mengshetty.vedant174@gmail.com>
2024-10-27 15:54:45 +0530

    Variables.C

--- Variables.C
+++ Variables.C
@@ -1,1 +1,28 @@
+#include<stdio.h>
+int main() 
+{
+     int a;
+     a=49;
+     //a is storing value which is 49//
+     printf("%d", a);
+     return 0;
+     
+     }
+
+
+
+Output:
+49
+[Process completed - press Enter]
+
+
+
+
+
+
+
+
+
+
+
 
