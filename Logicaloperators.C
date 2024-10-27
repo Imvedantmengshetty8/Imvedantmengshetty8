70d9ca00ad20cb46e43735e53ae49ba77240a49f
Imvedantmengshetty8 <mengshetty.vedant174@gmail.com>
2024-10-28 02:53:05 +0530

    Create Logicaloperators.C

---
+++ Logicaloperators.C
@@ -0,0 +1,68 @@
+//code for logical operators //
+#include <stdio.h>
+
+int main() {
+     int main;
+     int a,b;
+     a= 52;
+     b= 48;
+     
+     //operation for logical AND//
+    printf("a&&b=%d\n",a&&b);
+    //operation for logical OR //
+    printf("a||b=%d\n",a||b);
+    //operation for logical NOR //
+    printf("a!=b=%d\n",a!=b);
+    
+    
+    return 0;
+}
+
+
+
+Output:
+
+
+
+a&&b=1
+a||b=1
+a!=b=1
+
+[Process completed - press Enter]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
