b9d7cf19c47c18f92f61b1dd6124b93c5f0a8275
Imvedantmengshetty8 <mengshetty.vedant174@gmail.com>
2024-10-28 05:33:22 +0530

    Create Conditionaloperators.C

---
+++ Conditionaloperators.C
@@ -0,0 +1,93 @@
+//code for conditional operators //
+
+
+
+
+1.
+
+
+
+#include <stdio.h>
+
+int main() {
+     
+     int age;
+     //take input from users//
+     printf("enter your age :");
+     scanf(" %d", & age);
+     //ternary operator to find if a person can vote or not//
+     if
+     
+     
+     (age>=18);
+     
+     printf("you can vote");
+     
+     
+     
+     (age<18);
+     printf("you cannot vote");
+      
+    
+    
+    return 0;
+}
+
+
+
+Output:
+
+enter your age :20
+you can vote
+[Process completed - press Enter]
+
+
+
+
+
+
+
+
+
+2.
+
+
+/code for conditional operators //
+#include <stdio.h>
+
+int main(){
+     
+     int num =10;
+     //take input from users//
+     if(num %2==0)
+     {
+     printf(" EVEN number ");
+     }
+     else
+     {
+     printf("ODD number");
+     }
+     //ternary operator to find number is even or odd//
+     return 0;
+}
+
+
+
+Output:
+
+
+EVEN number
+[Process completed - press Enter]
+
+
+
+
+
+
+
+
+
+
+
+
+
